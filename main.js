c6b672011b211763d164af1c76d9bcaf1c1f36c7
Furkan003 <45043712+Furkan003@users.noreply.github.com>
2022-12-20 09:57:08 +0300

    general update

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var peer=new Peer();
 
 let localStream;
+let localId;
 
 const inputLocalPeerId=document.getElementById("myId");
 const inputRemotePeerId=document.getElementById("callId");
 const btnCall=document.getElementById("join");
 const btnScreen=document.querySelector(".screenBtn");
-const myId=document.getElementById("myId");
+const send_button=document.getElementById("send");
 
 
 navigator.mediaDevices.getUserMedia({video:true,audio:true}).then(stream=>{
@@ -17,17 +18,31 @@ navigator.mediaDevices.getUserMedia({video:true,audio:true}).then(stream=>{
 
 
 peer.on("open",id=>{
-    inputLocalPeerId.innerText=id;
+    localId=id;
 });
+
+function wpSend(){
+    let wpUrl;
+    if (inputRemotePeerId.value==="") {
+        wpUrl="https://api.whatsapp.com/send?phone=905455105084&text=moozjs.vercel.app/index.html?id="+localId
+    }else{
+        wpUrl="https://api.whatsapp.com/send?phone=90"+inputRemotePeerId.value+"&text=moozjs.vercel.app/index.html?id="+localId
+    }
+    
+    window.open(wpUrl,"_blank")
+}
 
 
 btnCall.addEventListener("click",()=>{
-    const remotePeerId=inputRemotePeerId.value;
-    const call=peer.call(remotePeerId,localStream);
-    call.on("stream",stream=>{
-        const remoteVideo=document.getElementById("remoteVideo")
-        createVideo(remoteVideo,stream)
-    })
+    if (getParamas()!==null) {
+        const call=peer.call(getParamas(),localStream);
+        call.on("stream",stream=>{
+            const remoteVideo=document.getElementById("remoteVideo")
+            createVideo(remoteVideo,stream)
+        })
+    }else{
+        console.log("gelmedi")
+    }
 })
 peer.on("call",(call)=>{
     call.answer(localStream);
@@ -36,7 +51,22 @@ peer.on("call",(call)=>{
         createVideo(remoteVideo,stream)
     })
 })
+
 function createVideo(videoEl,stream){
     videoEl.srcObject=stream;
     videoEl.onloadedmetadata=()=>videoEl.play()
+}
+const getParamas=()=>{
+    let urlString=(window.location.href).toLowerCase();
+    let url=new URL(urlString);
+    let remoteId=url.searchParams.get("id")
+    return remoteId;
+}
+window.onload=()=>{
+    if (getParamas()!==null) {
+        inputRemotePeerId.style.display="none";
+        document.getElementById("send").style.display="none"
+    }else{
+        btnCall.style.display="none"
+    }
 }
